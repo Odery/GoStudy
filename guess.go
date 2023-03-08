ab9a6dd8ba1fac1abed635ded2aa5d4fff55054b
Odery <kammurimini@gmail.com>
2023-03-08 15:08:00 +0200

    first commit

---
+++ guess.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+var numToGuess int = 42
+
+func mains(){
+	trysToGuess := 0
+	for{
+		guessed := rand.Intn(100) +1
+		if guessed == numToGuess{
+			fmt.Printf("The number %v was guessed corectly! In %v attempts",numToGuess, trysToGuess)
+			break
+		}else{
+			trysToGuess ++
+			if guessed < numToGuess{
+				fmt.Println("Guessed number is smaller!")
+			}else{
+				fmt.Println("Guessed number is bigger!")
+			}
+		}
+	}
+}
