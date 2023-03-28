41bb07681a665fb643df2c06e813f5e5f7023707
Odery <kammurimini@gmail.com>
2023-03-28 19:01:59 +0300

    structs and methods practice

---
+++ landing/location/location.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+
+)
+
+type coordinate struct{
+	degrees,minutes,seconds float64
+	hemisphere rune
+}
+
+func (c coordinate) decimal() float64{
+	sign := 1.0
+	switch c.hemisphere{
+	case 'S','W','s','w':
+		sign = -1.0
+	}
+	return sign * (c.degrees + c.minutes/60 + c.seconds/3600)
+}
+
+type location struct{
+	lat,lon float64
+}
+func main(){
+	spiritLat := coordinate{degrees: 14,minutes: 34,seconds: 6.2,hemisphere: 'S'}
+	spiritLon := coordinate{degrees: 175,minutes: 28,seconds: 21.5,hemisphere: 'E'}
+
+	oppoLat := coordinate{degrees: 1,minutes: 56,seconds: 46.3,hemisphere: 'S'}
+	oppoLon := coordinate{degrees: 354,minutes: 28,seconds: 24.2,hemisphere: 'E'}
+
+	curiosityLat := coordinate{degrees: 4,minutes: 35,seconds: 22.2,hemisphere: 'E'}
+	curiosityLon := coordinate{degrees: 137,minutes: 26,seconds: 30.1,hemisphere: 'E'}
+	
+	inSightLat := coordinate{degrees: 4,minutes: 30,seconds: 0,hemisphere: 'N'}
+	inSightLon := coordinate{degrees: 135,minutes: 54,seconds: 0,hemisphere: 'E'}
+}
+
+func NewLocation(lat, lon coordinate) location{
+	return location{lat.decimal(), lon.decimal()}
+}
