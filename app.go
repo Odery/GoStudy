cd082b602e92c043063214a37b1cd9d984e24fbb
Odery <kammurimini@gmail.com>
2023-03-31 18:32:04 +0300

    interfaces practice

--- app.go
+++ app.go
@@ -1,31 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type cat struct{
+type coordinate struct{
+	d,m,s float64
+	h rune
+}
+
+type location struct{
 	name string
+	lat, lon coordinate
 }
 
-func (c cat) voice(s string, count int){
-	for i :=0; i < count; i++{
-		fmt.Println(s)
-	}
-}
-
-type dog struct{
-	name string
-}
-
-func (d dog) voice(s string){
-	fmt.Println("Woof! ",s)
-}
-
-type zoo struct{
-	cat
-	dog
+func (l location) String() string{
+	return fmt.Sprintf("%s is at: %0.2fº%0.2f'%0.2f\" %c, %0.2fº%0.2f'%0.2f\" %c", l.name, l.lat.d, l.lat.m, l.lat.s, l.lat.h, l.lon.d,l.lon.m, l.lon.s, l.lon.h)
 }
 func main() {
-	z := zoo{cat{name: "Bob"},dog{name: "Jhon"}}
-
-	z.voice("Barq", 2)
+	l := location{name: " Elysium Planitia", lat: coordinate{d: 4, m: 30, h: 'N'}, lon: coordinate{d: 135, m: 54, h: 'E'} }
+	fmt.Println(l)
 }
