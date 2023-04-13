c4a60ef2d236d5d3ab5f64f4bda442cb28dab17a
Odery <kammurimini@gmail.com>
2023-04-13 12:02:05 +0300

    knight taks (nil practice)

--- app.go
+++ app.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-type talker interface{
-	talk() string
-}
-type laser int
-
-func (l laser) talk() string{
-	return (strings.Repeat("pew", int(l)))
-}
-func shout(t talker){
-	fmt.Println(strings.ToUpper(t.talk()))
-}
 func main() {
-	la := laser(2)
+	fmt.Println()
+	var i *int
 
-	shout(&la)
+	fmt.Println(*i + 1)
 }
