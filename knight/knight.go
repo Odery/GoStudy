fcefdf613bbba41a3e8e15b1b1378e09e15463c9
Odery <kammurimini@gmail.com>
2023-04-13 12:39:11 +0300

    knight task completed

--- knight/knight.go
+++ knight/knight.go
@@ -1,9 +1,35 @@
 package main
 
-import(
+import "fmt"
 
-)
+type item string
+
+type character struct{
+	name string
+	item *item
+}
+
+func (c *character) pickup(i *item){
+	c.item = i
+	fmt.Printf("%s has picked up the %s\n", c.name, *i)
+}
+
+func (c *character)giveItem(b *character){
+	if c.item != nil{
+		b.item = c.item
+		c.item = nil
+		fmt.Printf("%s has given %s to the %s\n", c.name, *b.item, b.name)
+	}else{
+		fmt.Println("No item in inventory")
+	}
+}
 
 func main(){
-	
+	arthur := character{name: "Arthur"}
+	lancelot := character{name: "Lancelot"}
+	sword := item("Long Sword")
+
+	arthur.pickup(&sword)
+
+	arthur.giveItem(&lancelot)
 }
