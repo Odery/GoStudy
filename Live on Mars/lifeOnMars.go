97bcdf5e72d8eb9fa99b733f6ad6ab0d3833ec40
Odery <kammurimini@gmail.com>
2023-05-01 16:02:38 +0300

    task completed

--- Live on Mars/lifeOnMars.go
+++ Live on Mars/lifeOnMars.go
@@ -1,60 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"image"
+	"log"
+	"math/rand"
 	"sync"
 	"time"
-	"math/rand"
 )
 
-type command int
-
-const (
-	right = command(0)
-	left = command(1)
+const(
+	gridHeight = 10
+	gridWidth = 25
 )
 
 func main() {
+	earth := NewEarth()
+	grid := NewMarsGrid()
+	
+	StartRover("Curiosity", grid, image.Point{X:1, Y:1}, earth.in)
+	StartRover("Oportunity", grid, image.Point{X:3, Y:3}, earth.in)
+	StartRover("Calisto", grid, image.Point{X:6, Y:6}, earth.in)
+	StartRover("Huan", grid, image.Point{X:8, Y:9}, earth.in)
 
-}
-
-//Rover type to run around the MarsGrid
-type Rover struct{
-	mu sync.Mutex
-	command chan command
-	mg *MarsGrid
-	move bool
-}
-
-//Constructor for a new Rover
-func NewRover(mg *MarsGrid) *Rover{
-	r := Rover{
-		command: make(chan command),
-		move: false,
-		mg: mg,
-	}
-
-	go r.Drive()
-	return &r
-}
-//Drive func for Rover to start movement
-func (r *Rover) Drive(){
-	next := time.After(250 * time.Millisecond)
 	for {
-		select{
-		case <- next:
-
-		}
+		report := <- earth.out
+		for _, s := range report {
+			log.Println(s)
+        }
 	}
 }
+ 
+type Cell struct{
+	mu sync.Mutex
+	chanceOfLife int
+	reported bool
+	Occupier
+}
 
-// MarsGrid represents a grid of some of the surface
-// of Mars. It may be used concurrently by different
-// goroutines.
-// It is pointless to add mutex field to whole Grid,
-// so the grid cell will have it instead
 type MarsGrid struct{
-	grid [100][100]Cell
+	grid [gridHeight][gridWidth]Cell
 }
 
 func NewMarsGrid() *MarsGrid{
@@ -64,6 +49,10 @@ func NewMarsGrid() *MarsGrid{
 		for j := range m.grid[i]{
 			m.grid[i][j] = Cell{
 				chanceOfLife: rand.Intn(1000) + 1,
+				Occupier: Occupier{
+					position: image.Point{i,j},
+					mg: &m,
+				},
 			}
 		}
 	}
@@ -71,35 +60,163 @@ func NewMarsGrid() *MarsGrid{
 	return &m
 }
 
-// Occupy occupies a cell at the given point in the grid. It
-// returns nil if the point is already occupied or the point is
-// outside the grid. Otherwise it returns a value that can be
-// used to move to different places on the grid
 func (m *MarsGrid) Occupy(p image.Point) *Occupier{
-	//TODO
-	return nil
+	cell := &m.grid[p.X][p.Y]
+
+	if cell.Occupier.occupied == false && inBounds(p){
+		cell.mu.Lock()
+		defer cell.mu.Unlock()
+
+		oc := cell.Occupier
+		oc.occupied = true
+		return &oc
+	}else{
+		return nil
+	}
 }
 
-// Grid cell to represent state of the grid
-type Cell struct{
-	mu sync.Mutex
-	chanceOfLife int
-	Occupier
+func inBounds(p image.Point) bool{
+	return p.X >= 0 && p.X < gridHeight && p.Y >= 0 && p.Y < gridWidth
 }
 
-// Occupier represents an occupied cell in the grid.
-// It may be used concurrently by different goroutines.
 type Occupier struct{
-	mu sync.Mutex
+	position image.Point
 	occupied bool
+	mg *MarsGrid
 }
 
-// Move moves the occupier to a different cell in the grid.
-// It reports whether the move was successful
-// It might fail because it was trying to move outside
-// the grid or because the cell it's trying to move into
-// is occupied. If it fails, the occupier remains in the same place.
 func (o *Occupier) Move(p image.Point) bool{
-	//TODO
+	var targetCell *Cell
+	if inBounds(p){
+		targetCell = &o.mg.grid[p.X][p.Y]
+	}else{
+		return false
+	}
+	if targetCell.occupied == false{
+		targetCell.mu.Lock()
+		defer targetCell.mu.Unlock()
+
+		currentCell := &o.mg.grid[o.position.X][o.position.Y]
+		currentCell.mu.Lock()
+		defer currentCell.mu.Unlock()
+
+		currentCell.occupied = false
+		targetCell.occupied = true
+
+		o.position = p
+		
+        return true
+	}
 	return false
+}
+
+type Rover struct{
+	mu sync.Mutex
+	name string
+	earth chan string
+	mg *MarsGrid
+	*Occupier
+}
+
+
+func StartRover(name string, mg *MarsGrid, startPoint image.Point, ch chan string){
+	r := Rover{
+		mg: mg,
+		name: name,
+		earth: ch,
+		Occupier: mg.Occupy(startPoint),
+	}
+	go r.Drive()
+}
+
+func (r *Rover) Drive(){
+	for {
+		next := time.After(250 * time.Millisecond)
+		select{
+		case <- next:
+			if ok, life := r.cellHasLife(); ok{
+				r.earth <- fmt.Sprintf("Rover: %v, found life in cell, life value is: %v. Cell coordinates: %v", r.name, life, r.position)
+			}
+			r.seekLife()
+		}
+	}
+}
+
+func (r *Rover) seekLife(){
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	
+	for moved := false; !moved; {
+		switch c := rand.Intn(4); c {
+			case 0:
+                moved = r.goLeft()
+			case 1:
+                moved = r.goRight()
+            case 2:
+                moved = r.goUp()
+            case 3:
+                moved = r.goDown()
+		}
+	}
+}
+
+func (r *Rover) goLeft() bool{
+	return r.Move(image.Point{X: r.Occupier.position.X, Y: (r.Occupier.position.Y - 1)})
+}
+
+func (r *Rover) goRight() bool{
+	return r.Move(image.Point{X: r.Occupier.position.X, Y: (r.Occupier.position.Y + 1)})
+}
+
+func (r *Rover) goDown() bool{
+	return r.Move(image.Point{X: (r.Occupier.position.X + 1), Y: r.Occupier.position.Y})
+}
+
+func (r *Rover) goUp() bool{
+	return r.Move(image.Point{X: (r.Occupier.position.X - 1), Y: r.Occupier.position.Y})
+}
+
+func (r *Rover) cellHasLife() (bool, int){
+	cell := &r.mg.grid[r.position.X][r.position.Y]
+	if !cell.reported {
+		cell.reported = true
+		return cell.chanceOfLife > 900, cell.chanceOfLife
+	}else{
+		return false, 0
+	}
+}
+
+type Earth struct{
+	in chan string
+	out chan []string
+}
+
+func (e *Earth) listenToRovers() {
+	report := make([]string, 0)
+	for {
+		next := time.After(1 * time.Second)
+        select {
+        case msg := <- e.in:
+            report = append(report, msg)
+		case <- next:
+			if len(report) > 0 {
+				log.Println("Data received from rovers")
+				e.out <- report
+				report = make([]string, 0)
+        	}else{
+				log.Println("No data received from rovers")
+			}
+    	}
+	}
+}
+
+func NewEarth() *Earth {
+	earth := Earth{
+		in: make(chan string),
+        out: make(chan []string),
+	}
+
+	go earth.listenToRovers()
+
+	return &earth
 }
--- app.go
+++ app.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"log"
-	"sync"
 )
 
 func main(){
-	log.Println("time.Millisecond")
 
-	g := MarsGrid{grid: [100][100]gridCell{},}
-
-	log.Println(g.grid[0])
+	h := Human{}
+	h.safeToWalk = true
+	log.Println(h)
 }
 
-//Mars Grid cell
-type gridCell struct{
-	lifeChance int
-	occupied bool
-
+type Human struct{
+	Legs
 }
 
-// MarsGrid represents a grid of some of the surface
-// of Mars. It may be used concurrently by different
-// goroutines.
-type MarsGrid struct{
-	mu sync.Mutex
-	grid [100][100]gridCell
+type Legs struct{
+	safeToWalk bool
 }
