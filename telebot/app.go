b05f9705a486066b980804631cb26d465277216d
Odery <kammurimini@gmail.com>
2023-06-01 17:14:35 +0300

    Added telebot

---
+++ telebot/app.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+//Declare the telebot token
+var token string
+
+//get the token
+func init(){
+	token = os.Getenv("BOT_TOKEN")
+}
+func main() {
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	b.Start()
+}
